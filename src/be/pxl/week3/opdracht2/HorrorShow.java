d1f2a4cc27e76b138f220fbc1c268d287bda1a8c
Gilles Leenders <gilles.leenders@gmail.com>
2018-08-23 11:36:11 +0200

    gefixt

--- src/be/pxl/week3/opdracht2/HorrorShow.java
+++ src/be/pxl/week3/opdracht2/HorrorShow.java
@@ -3,7 +3,36 @@ package be.pxl.week3.opdracht2;
 public class HorrorShow {
 
 	public static void main(String[] args) {
+		
+		class Vampire implements Monster {
+			public void menace() {
+				System.out.println("I'll drink your blood!!");
+			}
+		}
 
+		class Werewolf implements DangerousMonster {
+			
+			public void menace() {
+				System.out.println("I'll destroy you.");
+			}
+			
+			public void destroy(Monster monster) {
+				monster.menace();
+				System.out.println("Big fight...");
+				System.out.println(getClass().getSimpleName() + " killed one " + monster.getClass().getSimpleName());
+			}
+		}
+		
+		Werewolf werewolf = new Werewolf();
+		Vampire vampire = new Vampire();
+		
+		werewolf.destroy(vampire);
+		werewolf.destroy(new Monster() {
+			@Override
+			public void menace() {
+				System.out.println("You smell like wet dog.");
+			}
+		});
 	}
 
 }
